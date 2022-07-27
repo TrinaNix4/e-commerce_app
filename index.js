5449c044c0a44682e778e73febea1fcb190f6798
Trina Nixon <TrinaNixon7@gmail.com>
2022-07-27 17:21:13 -0600

    set up root route and listening

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hi there!");
+//require in express
+const express = require("express");
+
+const app = express();
+
+//when user makes a request to root route (get('/'))
+//want to run callback function  and take the string 'hi there' and send it back to whoever just made the request
+
+//tells our web server what it should do when it receives a network request from our browswer
+//object req - request  res - response
+//will find info from user in this req object
+//send info back to browser - interact with res object
+app.get("/", (req, res) => {
+  res.send("hi there!");
+});
+
+//listen for incoming network traffic
+app.listen(3000, () => {
+  console.log("listening");
+});
